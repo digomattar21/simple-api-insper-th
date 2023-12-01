5ce5dddb4ca5134681bf77b47e9e69dc9cf152a4
Digo <digomattar@gmail.com>
2023-12-01 19:36:43 -0300

    fix: port number

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 require('dotenv').config();
 const app = express();
-const port = 80;
+const port = 8080;
 
 const pool = new Pool({
     user: process.env.DB_USER,
